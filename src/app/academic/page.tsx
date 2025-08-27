6787354d0d2199387b33f0f012f27e7adea06174
Siddhant Vats <siddhantvats840@gmail.com>
2025-08-27 16:29:49 +0530

    comit

--- src/app/academic/page.tsx
+++ src/app/academic/page.tsx
@@ -5,16 +5,12 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 import Academic from "./components/Academic";
 
-
-
 export default function Home() {
   return (
     <>
-    <div className="bg-[#F9FAFB] text-[#111827]">
-    <Academic></Academic>
-     
-      
-    </div>
+      <div className="bg-[#F9FAFB] text-[#111827]">
+        <Academic></Academic>
+      </div>
     </>
   );
 }
